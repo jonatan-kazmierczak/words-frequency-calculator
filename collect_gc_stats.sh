710cb6e8cf5584304405df0ad99ee1e546a329cf
Jonatan Kazmierczak <jonatan.kazmierczak@trivadis.com>
2020-01-28 01:17:58 +0100

    Update collect_gc_stats.sh

--- collect_gc_stats.sh
+++ collect_gc_stats.sh
@@ -17,7 +17,7 @@ JAVA_ZING_CMD=/opt2/zing-jdk11.0.0-19.10.1.0-3/bin/java
 REPEAT_COUNT=11
 
 echo "-- Java GC Performance Statistics Collector --"
-echo "Please make sure, that you are not running it in virtualized environment (i.e. Docker) and that your laptop works in performance mode"
+echo "Please make sure, that you are not running it in virtualized environment (i.e. Docker) and that your CPU runs on a constant frequency"
 echo
 
 $JAVA_CMD -version
@@ -29,76 +29,49 @@ OUT_FILE=$STATS_DIR/gc.txt
 # HotSpot
 
 echo -n vm=HotSpot,gc=Epsilon, > $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Serial, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseSerialGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseSerialGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Parallel, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseParallelGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseParallelGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=G1, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseG1GC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseG1GC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Z, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Shenandoah, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 # OpenJ9
 
 echo -n vm=OpenJ9,gc=nogc, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:nogc -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:nogc -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=gencon, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:gencon -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:gencon -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=balanced, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:balanced -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:balanced -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=optavgpause, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:optavgpause -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optavgpause -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=optthruput, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:optthruput -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optthruput -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=metronome, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -Xgcpolicy:metronome -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:metronome -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 # Zing
 
 echo -n vm=Zing,gc=C4, >> $OUT_FILE
-/bin/time $JAVA_ZING_CMD -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_ZING_CMD -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
 
 # SVM
 
 echo -n vm=Substrate,gc=SVM, >> $OUT_FILE
-/bin/time ./words-freq-calc CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
-
-exit 0
-
-echo
-echo "Parallel version"
-OUT_FILE=$STATS_DIR/gc_prl.txt
-
-echo -n gc=Epsilon, > $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
-
-echo -n gc=G1, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseG1GC -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
-
-echo -n gc=Parallel, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseParallelGC -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
-
-echo -n gc=Serial, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UseSerialGC -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
-
-echo -n gc=Z, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
-
-echo -n gc=Shenandoah, >> $OUT_FILE
-/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
-
-echo -n gc=gencon, >> $OUT_FILE
-/bin/time $JAVA_J9_CMD -jar build/libs/words-freq-calc.jar CalculatorJ8Prl $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time ./words-freq-calc CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
