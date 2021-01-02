d5a1a4ba3c17e9c4f8e3d652651d17dc46836e52
Jonatan Kazmierczak <jonatan.kazmierczak@trivadis.com>
2021-01-02 23:05:04 +0100

    Update collect_*_stats.sh

--- collect_gc_stats.sh
+++ collect_gc_stats.sh
@@ -6,15 +6,16 @@
 # define variables
 STATS_DIR=stats
 TEXT_FILE=texts/bible_ylt.txt
-#TEXT_FILE=texts/4_gospels.txt
-# Path to java command from OpenJDK build including Shenandoah GC - i.e. AdoptOpenJDK
-JAVA_CMD=/opt2/jdk-13.0.1+9/bin/java
+REPEAT_COUNT=21
+JAVA_APP="-jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT"
+# Path to java command from OpenJDK build
+JAVA_CMD=/opt2/jdk-15.0.1+9_hotspot/bin/java
 # Path to java command from OpenJDK with OpenJ9 VM
-JAVA_J9_CMD=/opt2/jdk-13.0.1+9_openj9/bin/java
+JAVA_J9_CMD=/opt2/jdk-15.0.1+9_openj9/bin/java
 # Path to java command from Zing VM
-JAVA_ZING_CMD=/opt2/zing-jdk11.0.0-19.10.1.0-3/bin/java
-
-REPEAT_COUNT=11
+JAVA_ZING_CMD=/opt2/zing20.12.0.0-5-ca-jdk11.0.9-linux_x64/bin/java
+# Path to java command from GraalVM
+JAVA_GRAALVM_CMD=/opt2/graalvm-ce-java11-20.3.0/bin/java
 
 echo "-- Java GC Performance Statistics Collector --"
 echo "Please make sure, that you are not running it in virtualized environment (i.e. Docker) and that your CPU runs on a constant frequency"
@@ -29,49 +30,51 @@ OUT_FILE=$STATS_DIR/gc.txt
 # HotSpot
 
 echo -n vm=HotSpot,gc=Epsilon, > $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Serial, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UseSerialGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseSerialGC $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Parallel, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UseParallelGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseParallelGC $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=G1, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UseG1GC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT  2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseG1GC $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Z, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseZGC $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=HotSpot,gc=Shenandoah, >> $OUT_FILE
-/usr/bin/time $JAVA_CMD -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_CMD -XX:+UseShenandoahGC $JAVA_APP 2>>$OUT_FILE
 
 # OpenJ9
 
 echo -n vm=OpenJ9,gc=nogc, >> $OUT_FILE
-/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:nogc -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:nogc $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=gencon, >> $OUT_FILE
-/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:gencon -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:gencon $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=balanced, >> $OUT_FILE
-/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:balanced -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:balanced $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=optavgpause, >> $OUT_FILE
-/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optavgpause -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optavgpause $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=optthruput, >> $OUT_FILE
-/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optthruput -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:optthruput $JAVA_APP 2>>$OUT_FILE
 
 echo -n vm=OpenJ9,gc=metronome, >> $OUT_FILE
-/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:metronome -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_J9_CMD -Xgcpolicy:metronome $JAVA_APP 2>>$OUT_FILE
 
 # Zing
-
 echo -n vm=Zing,gc=C4, >> $OUT_FILE
-/usr/bin/time $JAVA_ZING_CMD -jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
+/usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
 
-# SVM
+# GraalVM
+echo -n vm=GraalVM,gc=G1, >> $OUT_FILE
+/usr/bin/time $JAVA_GRAALVM_CMD $JAVA_APP 2>>$OUT_FILE
 
-echo -n vm=Substrate,gc=SVM, >> $OUT_FILE
+# Substrate
+echo -n vm=Substrate,gc=none, >> $OUT_FILE
 /usr/bin/time ./words-freq-calc CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
--- collect_jvm_stats.sh
+++ collect_jvm_stats.sh
@@ -6,15 +6,14 @@
 # define variables
 STATS_DIR=stats
 TEXT_FILE=texts/bible_ylt.txt
-#TEXT_FILE=texts/4_gospels.txt
 REPEAT_COUNT=30
 JAVA_APP="-jar build/libs/words-freq-calc.jar CalculatorJ8 $TEXT_FILE $REPEAT_COUNT"
 # Path to java command from OpenJDK build
-JAVA_CMD=java
+JAVA_CMD=/opt2/jdk-15.0.1+9_hotspot/bin/java
 # Path to java command from OpenJDK with OpenJ9 VM
 JAVA_J9_CMD=/opt2/jdk-15.0.1+9_openj9/bin/java
 # Path to java command from Zing VM
-JAVA_ZING_CMD=/opt2/zing20.10.0.0-4-ca-jdk11.0.8.0.101-linux_x64/bin/java
+JAVA_ZING_CMD=/opt2/zing20.12.0.0-5-ca-jdk11.0.9-linux_x64/bin/java
 # Path to java command from GraalVM
 JAVA_GRAALVM_CMD=/opt2/graalvm-ce-java11-20.3.0/bin/java
 
@@ -36,13 +35,13 @@ echo -n vm=OpenJ9,gc=gencon, >> $OUT_FILE
 /usr/bin/time $JAVA_J9_CMD $JAVA_APP 2>>$OUT_FILE
 
 # Zing
-#echo -n vm=Zing,gc=C4, >> $OUT_FILE
-#/usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
+echo -n vm=Zing,gc=C4, >> $OUT_FILE
+/usr/bin/time $JAVA_ZING_CMD $JAVA_APP 2>>$OUT_FILE
 
 # HotSpot (GraalVM)
-echo -n vm=Graal,gc=G1, >> $OUT_FILE
+echo -n vm=GraalVM,gc=G1, >> $OUT_FILE
 /usr/bin/time $JAVA_GRAALVM_CMD $JAVA_APP 2>>$OUT_FILE
 
 # Substrate
-echo -n vm=Substrate,gc=SVM, >> $OUT_FILE
+echo -n vm=Substrate,gc=none, >> $OUT_FILE
 /usr/bin/time ./words-freq-calc CalculatorJ8 $TEXT_FILE $REPEAT_COUNT 2>>$OUT_FILE
